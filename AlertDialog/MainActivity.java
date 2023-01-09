f9d89f04c495f10754813a5c4b76de2eac40d4c4
Majid Saqr <104238392+majidsaqr12@users.noreply.github.com>
2023-01-09 16:56:28 +0200

    Add files via upload

---
+++ AlertDialog/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.alertdialog;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+    private AlertDialog.Builder alertdialog;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                alertdialog = new AlertDialog.Builder(MainActivity.this);
+                alertdialog.setTitle(getResources().getString(R.string.title_alert));
+                alertdialog.setMessage(getResources().getString(R.string.massage));
+                alertdialog.setCancelable(false);
+                alertdialog.setPositiveButton(getResources().getString(R.string.yes), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        MainActivity.this.finish();
+                    }
+                });
+                alertdialog.setNegativeButton(getResources().getString(R.string.no), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel();
+                    }
+                });
+
+                AlertDialog dialog = alertdialog.create();
+                dialog.show();
+            }
+        });
+    }
+}
